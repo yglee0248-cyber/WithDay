fb60ef534d496999bdb3b7ef42d3173161f6d89d
yglee <yglee0248@gmail.com>
2026-06-16 16:47:22 +0900

    Summary

--- WithDayBack/src/main/java/com/test/withdayback/common/config/CloudinaryConfig.java
+++ WithDayBack/src/main/java/com/test/withdayback/common/config/CloudinaryConfig.java
@@ -5,7 +5,7 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+ 
 @Configuration
 public class CloudinaryConfig {
 
--- WithDayFront/vite.config.js
+++ WithDayFront/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     // 구글 로그인 팝업 에러(COOP) 해결을 위한 보안 헤더 출입증 추가
     headers: {
-      "Cross-Origin-Opener-Policy": "same-origin-allow-popups", 
-    }
-  }
-})
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
+  },
+});
